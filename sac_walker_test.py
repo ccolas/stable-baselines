3743379a450ecabf6a759da1223e428e114874a7
rPortelas <remy.portelas@gmail.com>
2019-02-22 10:07:21 +0100

    fix env name

--- sac_walker_test.py
+++ sac_walker_test.py
@@ -10,7 +10,7 @@ import click
 #import cProfile
 
 @click.command()
-@click.option('--env', type=str, default='BipedalWalker-v2', help='the name of the OpenAI Gym environment that you want to train on')
+@click.option('--env_name', type=str, default='BipedalWalker-v2', help='the name of the OpenAI Gym environment that you want to train on')
 @click.option('--logdir', type=str, default='sac_default_log/', help='the directory to where data should go.')
 @click.option('--exp_name', type=str, default='default', help='the file to where data should go.')
 @click.option('--n_env', type=int, default=1, help='the number of vectorized envs')
@@ -19,9 +19,9 @@ import click
 @click.option('--total_timesteps', type=int, default=100000, help='number of total environment steps')
 @click.option('--nb_eval_rollouts', type=int, default=10, help='number of total eval episodes')
 @click.option('--batch_size', type=int, default=64, help='number of batches per upgrade')
-def main(env, logdir, n_env, buffer_size, eval_freq, total_timesteps, nb_eval_rollouts, exp_name, batch_size):
+def main(env_name, logdir, n_env, buffer_size, eval_freq, total_timesteps, nb_eval_rollouts, exp_name, batch_size):
     print("launching sac with env {}, logs in {}, n_env {} bufsize {} eval_freq {} total_timesteps {} nb eval rols {} e_name {} btchsize {}"
-          .format(env, logdir, n_env, buffer_size, eval_freq, total_timesteps, nb_eval_rollouts, exp_name, batch_size))
+          .format(env_name, logdir, n_env, buffer_size, eval_freq, total_timesteps, nb_eval_rollouts, exp_name, batch_size))
     #
     # BipedalWalker-v2:
     #   n_timesteps: !!float 1e6
@@ -37,36 +37,36 @@ def main(env, logdir, n_env, buffer_size, eval_freq, total_timesteps, nb_eval_ro
     # cp = cProfile.Profile()
     # cp.enable()
 
-    env = SubprocVecEnv([lambda: gym.make('BipedalWalker-v2') for _ in range(n_env)])
-    eval_env = SubprocVecEnv([lambda: gym.make('BipedalWalker-v2') for _ in range(n_env)])
-
-    model = SAC(MlpPolicy, env, eval_env, verbose=1, learning_starts=1000, tensorboard_log="./sac_walker_tensorboard/",
-                replay_buffer=ReplayBuffer(buffer_size), eval_freq=eval_freq, nb_eval_rollouts=nb_eval_rollouts,
-                learning_rate=0.0003, batch_size=batch_size, logdir=logdir+exp_name)
-    model.learn(total_timesteps=total_timesteps)
-    model.save("sac_walker")
+    # train_env = SubprocVecEnv([lambda: gym.make(env) for _ in range(n_env)])
+    # eval_env = SubprocVecEnv([lambda: gym.make(env) for _ in range(n_env)])
+    #
+    # model = SAC(MlpPolicy, train_env, eval_env, verbose=1, learning_starts=1000, tensorboard_log="./sac_walker_tensorboard/",
+    #             replay_buffer=ReplayBuffer(buffer_size), eval_freq=eval_freq, nb_eval_rollouts=nb_eval_rollouts,
+    #             learning_rate=0.0003, batch_size=batch_size, logdir=logdir+exp_name)
+    # model.learn(total_timesteps=total_timesteps)
+    # model.save("sac_walker")
 
     # cp.disable()
     # cp.dump_stats("test_sac_1env.cprof")
 
     # del model # remove to demonstrate saving and loading
     #
-    # n_env = 1
-    # env = DummyVecEnv([lambda: gym.make('BipedalWalker-v2')])
-    # eval_env = DummyVecEnv([lambda: gym.make('BipedalWalker-v2')])
-    # model = SAC.load("sac_walker", env=env, eval_env=eval_env)
-    #
-    #
-    # obs = env.reset()
-    # r = 0
-    # while True:
-    #     action, _states = model.predict(obs)
-    #     obs, rewards, dones, info = env.step(action)
-    #     r+=rewards[0]
-    #     if dones[0]:
-    #         print(r)
-    #         r = 0
-    #     env.render()
+    n_env = 1
+    env = DummyVecEnv([lambda: gym.make(env_name)])
+    eval_env = DummyVecEnv([lambda: gym.make(env_name)])
+    model = SAC.load("sac_walker", env=env, eval_env=eval_env)
+
+
+    obs = env.reset()
+    r = 0
+    while True:
+        action, _states = model.predict(obs)
+        obs, rewards, dones, info = env.step(action)
+        r+=rewards[0]
+        if dones[0]:
+            print(r)
+            r = 0
+        env.render()
 
 if __name__ == "__main__":
     main()
