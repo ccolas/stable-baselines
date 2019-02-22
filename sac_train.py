eadadf18a7f818b1192673264260ce4118468631
rPortelas <remy.portelas@gmail.com>
2019-02-22 17:23:06 +0100

    add policy ent coef lr l_starts arguments

--- sac_train.py
+++ sac_train.py
@@ -8,6 +8,7 @@ from stable_baselines.common.replay_buffer import ReplayBuffer
 from stable_baselines.common.vec_env import SubprocVecEnv
 import click
 import os
+import tensorflow as tf
 #import cProfile
 
 @click.command()
@@ -21,13 +22,27 @@ import os
 @click.option('--nb_eval_rollouts', type=int, default=10, help='number of total eval episodes')
 @click.option('--batch_size', type=int, default=64, help='number of batches per upgrade')
 @click.option('--gpu_id', type=int, default=-1, help='specific gpu to focus computation on')
-def main(env_name, logdir, n_envs, buffer_size, eval_freq, total_timesteps, nb_eval_rollouts, exp_name, batch_size, gpu_id):
-    print("launching sac with env {}, logs in {}, n_envs {} bufsize {} eval_freq {} total_timesteps {} nb eval rols {} e_name {} btchsize {} gpu {}"
-          .format(env_name, logdir, n_envs, buffer_size, eval_freq, total_timesteps, nb_eval_rollouts, exp_name, batch_size, gpu_id))
+@click.option('--policy', type=str , default="small", help='specify if we want to use a big or small policy')
+@click.option('--ent_coef', type=float , default=-1, help='specify entropy coef')
+@click.option('--lr', type=float , default=0.0003, help='learning rate')
+@click.option('--l_starts', type=int , default=1000, help='learning starts: random initial actions to bootstrap learning')
+def main(env_name, logdir, n_envs, buffer_size, eval_freq, total_timesteps, nb_eval_rollouts, exp_name, batch_size, gpu_id, policy, ent_coef, lr, l_starts):
+    print("launching sac with env {}, logs in {}, n_envs {} bufsize {} eval_freq {} total_timesteps {} nb eval rols {} e_name {} btchsize {} gpu {} policy {} entcoef {} lr {} lsts {}"
+          .format(env_name, logdir, n_envs, buffer_size, eval_freq, total_timesteps, nb_eval_rollouts, exp_name, batch_size, gpu_id, policy, ent_coef, lr, l_starts))
 
     if gpu_id != -1:
         os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
 
+    if policy == "small":
+        policy_kwargs = dict(layers=[64, 64], act_fun=tf.nn.relu)
+    elif policy == "big":
+        policy_kwargs = dict(layers=[400, 300], act_fun=tf.nn.relu)
+    else:
+        print("unknown policy: {}".format(policy))
+        exit(0)
+
+    if ent_coef == -1:
+        ent_coef = 'auto'
 
     #
     # BipedalWalker-v2:
@@ -47,9 +62,10 @@ def main(env_name, logdir, n_envs, buffer_size, eval_freq, total_timesteps, nb_e
     env = SubprocVecEnv([lambda: gym.make(env_name) for _ in range(n_envs)])
     eval_env = SubprocVecEnv([lambda: gym.make(env_name) for _ in range(n_envs)])
 
-    model = SAC(MlpPolicy, env, eval_env, verbose=1, learning_starts=1000, tensorboard_log="./sac_walker_tensorboard/",
+    model = SAC(MlpPolicy, env, eval_env, verbose=1, learning_starts=l_starts, tensorboard_log="./sac_walker_tensorboard/",
                 replay_buffer=ReplayBuffer(buffer_size), eval_freq=eval_freq, nb_eval_rollouts=nb_eval_rollouts,
-                learning_rate=0.0003, batch_size=batch_size, logdir=logdir+exp_name)
+                learning_rate=lr, batch_size=batch_size, logdir=logdir+exp_name, policy_kwargs=policy_kwargs,
+                ent_coef=ent_coef)
     model.learn(total_timesteps=total_timesteps)
     model.save("sac_walker")
 
