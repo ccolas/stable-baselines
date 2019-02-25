c1deaeff71611c981046bb89a71e79b0ca448052
Remy PORTELAS <rportela@devel11.plafrim.cluster>
2019-02-25 18:41:00 +0100

    improve bash generator

--- bash_generator.py
+++ bash_generator.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-PATH_TO_INTERPRETER = "~/anaconda3/envs/tfGPU/bin/python"  # plafrim
-filename = "run_sac_walker_1_env.sh"
+PATH_TO_INTERPRETER = "/home/rportela/anaconda3/envs/tfGPU/bin/python"  # plafrim
+logdir_name = "sac_walker_newparam_ent005"
+filename = "run_" + logdir_name + ".sh"
 save_dir = '.'
-trial_id = list(range(0, 20))
+trial_id = list(range(0, 8))
 
 with open(filename, 'w') as f:
     f.write('#!/bin/sh\n')
@@ -15,7 +16,11 @@ with open(filename, 'w') as f:
     f.write('rm log.txt; \n')
     f.write("export EXP_INTERP='%s' ;\n" % PATH_TO_INTERPRETER)
 
+    gpu_id = 0
     for seed in range(len(trial_id)):
         t_id = trial_id[seed]
-        f.write("$EXP_INTERP sac_walker_test.py --logdir sac_walker_1_env/ --exp_name %s --n_env 1 --buffer_size 10000 --eval_freq 100 --total_timesteps 1000 --nb_eval_rollouts 10 &\n" % (t_id))
+        f.write("$EXP_INTERP sac_train.py --logdir %s/ --exp_name %s --env_name BipedalWalker-v2 --n_envs 1 --buffer_size 100000 --eval_freq 100000 --total_timesteps 1000000 --nb_eval_rollouts 10 --batch_size 100 --policy big --ent_coef 0.005 --lr 0.001 --l_starts 10000 --gpu_id %s &\n" % (logdir_name, t_id, gpu_id))
+        gpu_id += 1
+        if gpu_id == 4:
+            gpu_id = 0
     f.write('wait')
